bf7fc6d0a9d107e8683818a0189e362f7a57a97f
Maxim Egorov <maxgegorov@gmail.com>
2016-05-26 10:49:21 -0700

    initial commit

--- src/POMDPBounds.jl
+++ src/POMDPBounds.jl
@@ -1,5 +1,23 @@
 module POMDPBounds
 
-# package code goes here
+using POMDPs
+
+export
+    # Abstract type
+    Bound,
+
+    # Bound functions
+    upper_bound,
+    lower_bound,
+    initial_upper_bound,
+    initial_lower_bound
+
+abstract Bound
+
+POMDPs.@pomdp_func upper_bound{B}(bound::Bound, prob::Union{POMDP,MDP}, b::B)
+POMDPs.@pomdp_func lower_bound{B}(bound::Bound, prob::Union{POMDP,MDP}, b::B)
+
+POMDPs.@pomdp_func initial_upper_bound(bound::Bound, prob::Union{POMDP,MDP})
+POMDPs.@pomdp_func initial_lower_bound(bound::Bound, prob::Union{POMDP,MDP})
 
 end # module
